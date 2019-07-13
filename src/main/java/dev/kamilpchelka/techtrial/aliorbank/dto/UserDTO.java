d3e300e3283ef520dca1cb8d8d13b95d49da7d63
Kamil Pchelka <kamil.pchelka@gmail.com>
2019-07-13 19:24:42 +0200

    Add missing fields and constructors.

--- src/main/java/dev/kamilpchelka/techtrial/aliorbank/dto/UserDTO.java
+++ src/main/java/dev/kamilpchelka/techtrial/aliorbank/dto/UserDTO.java
@@ -2,8 +2,26 @@ package dev.kamilpchelka.techtrial.aliorbank.dto;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 @Data
 public class UserDTO {
     String firstName;
     String lastName;
+    Date dateOfBirth;
+    BigDecimal balance;
+
+    public UserDTO(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+    public UserDTO(String firstName, String lastName, Date dateOfBirth, BigDecimal balance) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.balance = balance;
+    }
 }
