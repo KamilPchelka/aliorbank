6070f25e0da0705171edadac5a04f0ddd44a9839
Kamil Pchelka <kamil.pchelka@gmail.com>
2019-07-13 11:10:59 +0200

    Add initial project version.
    The setup was generated using https://start.spring.io/

---
+++ src/main/java/dev/kamilpchelka/techtrial/aliorbank/TechTrialApplication.java
@@ -0,0 +1,13 @@
+package dev.kamilpchelka.techtrial.aliorbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTrialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechTrialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kamilpchelka/techtrial/aliorbank/TechTrialApplicationTests.java
@@ -0,0 +1,16 @@
+package dev.kamilpchelka.techtrial.aliorbank;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TechTrialApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
