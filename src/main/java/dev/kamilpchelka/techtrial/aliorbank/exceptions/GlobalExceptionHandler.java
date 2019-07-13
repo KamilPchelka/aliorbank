53bc29a1e8f2bdd84b48563ee18510c20320662e
Kamil Pchelka <kamil.pchelka@gmail.com>
2019-07-13 19:22:41 +0200

    Add missing annotation.

--- src/main/java/dev/kamilpchelka/techtrial/aliorbank/exceptions/GlobalExceptionHandler.java
+++ src/main/java/dev/kamilpchelka/techtrial/aliorbank/exceptions/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.AbstractMap;
 
@@ -15,6 +16,7 @@ public class GlobalExceptionHandler {
 
     private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
+    @ExceptionHandler
     public ResponseEntity<AbstractMap.SimpleEntry<String, String>> handle(Exception e) {
         log.error("Exception: Unable to process this request.", e);
         AbstractMap.SimpleEntry<String, String> response =
